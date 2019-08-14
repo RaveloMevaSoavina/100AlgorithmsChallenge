8826db12a023d47cd0c89cc76fbb2d840750cf68
Daniel Strong <danielstrong@mac.com>
2019-08-14 08:41:58 -0800

    complete addBorder function

--- addBorder/addBorder.ts
+++ addBorder/addBorder.ts
@@ -1,5 +1,11 @@
 function addBorder(picture: string[]): string[] {
-
+  const length = picture[0].length;
+  picture.unshift("*".repeat(length + 2));
+  for (let i = 0; i < picture.length; i++){
+    picture[i] = "*" + picture[i] + "*";
+  }
+  picture.push("*".repeat(length + 2));
+  return picture;
 }
 
-// console.log(addBorder(["abc", "ded"]));
+console.log(addBorder(["abc", "ded"]));
