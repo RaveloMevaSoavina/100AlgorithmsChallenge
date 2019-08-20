38943ab484ac57001594e389635ef0bb286c173b
Daniel Strong <danielstrong@mac.com>
2019-08-19 16:16:14 -0800

    complete adjacentElementProduct

--- adjacentElementsProduct/adjacentElementsProduct.ts
+++ adjacentElementsProduct/adjacentElementsProduct.ts
@@ -1,5 +1,13 @@
 function adjacentElementsProduct(inputArray: number[]): number {
-
+  let max = inputArray[0] * inputArray[1];
+  let product = 0;
+  for (let i = 1; i < inputArray.length - 1; i++){
+    product = inputArray[i] * inputArray[i + 1]
+    if (multiplied > max){
+        max = multiplied;
+    }
+  }
+  return multiplied;
 }
 
-//console.log(adjacentElementsProduct([3, 6, -2, -5, 7, 3]));
+console.log(adjacentElementsProduct([3, 6, -2, -5, 7, 3]));
