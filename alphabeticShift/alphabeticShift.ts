f180550cfb1db8b54eb800d845e1789a03261273
Daniel Strong <danielstrong@mac.com>
2019-08-22 08:32:00 -0800

    complete alphabetShift

--- alphabeticShift/alphabeticShift.ts
+++ alphabeticShift/alphabeticShift.ts
@@ -1,5 +1,14 @@
 function alphabeticShift(inputString: string): string {
-
+  let str = inputString.split('');
+  let newStr = [];
+  for (let i = 0; i < str.length; i++){
+    if (str[i] === 'z'){
+      newStr.push('a');
+    } else {
+      newStr.push(String.fromCharCode(str[i].charCodeAt() + 1));
+    }
+  }
+  return newStr.join('');
 }
 
 console.log(alphabeticShift('crazy'));
