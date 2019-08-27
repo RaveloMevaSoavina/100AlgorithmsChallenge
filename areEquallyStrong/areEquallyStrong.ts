6ae21d3c8012de4dda19f1186719ed47e5b82651
Daniel Strong <danielstrong@mac.com>
2019-08-27 08:12:00 -0800

    add areEquallyStrong

--- areEquallyStrong/areEquallyStrong.ts
+++ areEquallyStrong/areEquallyStrong.ts
@@ -1,7 +1,13 @@
 function areEquallyStrong(yourLeft: number, yourRight: number, friendsLeft: number, friendsRight: number): boolean {
-
+  let me = arguments[0] + arguments[1];
+  let friend = arguments[2] + arguments[3];
+  if (me === friend){
+      return true;
+  } else {
+      return false;
+  }
 }
 
-// console.log(areEquallyStrong(10, 15, 15, 10))
-// console.log(areEquallyStrong(15, 10, 15, 10))
-// console.log(areEquallyStrong(15, 10, 15, 9))
+console.log(areEquallyStrong(10, 15, 15, 10))
+console.log(areEquallyStrong(15, 10, 15, 10))
+console.log(areEquallyStrong(15, 10, 15, 9))
